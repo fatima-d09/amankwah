c2547dd3684bbe1f91c4b5a34f36975f2974eb36
fatima-d09 <fdembele@u.rochester.edu>
2024-04-23 21:28:10 +0500

    completed

---
+++ src/pages/booking/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import './Booking.css'
+const Booking = () => {
+  
+  return (
+    <center>
+      <Link to={'https://calendly.com/yaaontb/30min?month=2024-04'}>
+      <img src="https://i.imgur.com/Lz8dg64.png" height={700}  />
+      </Link>
+    </center>
+
+  );
+};
+
+export default Booking;
